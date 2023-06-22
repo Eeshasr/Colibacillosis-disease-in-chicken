fdbafe8bc2fcbe1165ee11ee14e7a3ce6ce8518b
Eeshsr <eesharavindran@gmail.com>
2023-06-22 13:45:14 +0200

    data ingestion done

---
+++ README.md
@@ -0,0 +1,11 @@
+# Colibacillosis-disease-in-chicken
+## workflows
+update config.yaml
+update secrets.yaml
+update parmas.yaml
+update the entity
+update the configuration manager in the src() config
+update the components
+update the pipeline
+update the main.py
+update the dvc.yaml
---
+++ src()/CNNCLASIFIER/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,25 @@
+from CNNCLASIFIER.config.configuration.configuration import ConfigurationManager
+from CNNCLASIFIER.components.Data_ingestion import DataIngestion
+from CNNCLASIFIER import logger
+
+STAGE_NAME = "Data_ingestion_stage"
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+    def main(self):
+      config = ConfigurationManager()
+      data_ingestion_config = config.get_data_ingestion_config()
+      data_ingestion = DataIngestion(config=data_ingestion_config)
+      data_ingestion.download_file()
+      data_ingestion.extract_zip_file()
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
